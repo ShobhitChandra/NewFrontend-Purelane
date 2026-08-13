ff2bb961286ec68971d31eb86078d5bcfb409da7
ShobhitChandra <Shobhitvchandra@gmail.com>
2026-08-13 11:52:08 +0530

    feat: add bundle, combo and footer components
    - Create Bundle component for product bundles
    - Add Best Selling combo section
    - Implement Footer with links and info
    - Complete landing page structure

---
+++ frontend/src/Landing_Page/Best_Selling_combo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Best_Selling_combo() {
+    return (
+        <div>
+            Best_Selling_combo
+        </div>
+    )
+}
+
+export default Best_Selling_combo;
---
+++ frontend/src/Landing_Page/Bundle.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Bundle() {
+    return (
+        <div>
+            Bundle
+        </div>
+    )
+}
+
+export default Bundle;
